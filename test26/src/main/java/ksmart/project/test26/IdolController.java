60be1f640fce58606f08f65937ec77feaadfdef5
안영신 <@DESKTOP-UQUOB9R>
2018-01-31 14:43:33 +0900

    (안영신)IdolController.java 생성 후 idolList()메서드선언 /idol/idolList로 요청이오면 Dao를호출후 모든 내용을 셋팅하고 idolList.jsp로 포워딩한다.

---
+++ test26/src/main/java/ksmart/project/test26/IdolController.java
@@ -0,0 +1,23 @@
+package ksmart.project.test26;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ksmart.project.test26.service.Idol;
+import ksmart.project.test26.service.IdolDao;
+
+@Controller
+public class IdolController {
+	@Autowired
+	private IdolDao idolDao;
+	@RequestMapping(value="/idol/idolList")
+	public String idolList(Model model) {
+		List<Idol> list = idolDao.selectIdolList();
+		model.addAttribute("list", list);
+		return "idol/idolList";
+	}
+}
