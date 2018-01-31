81ef386d7423062d0ef2070be72ac03f6b48034d
Administrator <Administrator@smart20105>
2018-01-31 12:41:45 +0900

    (임승권) CityController 생성 및 ctiyList() 메서드 생성, /city/cityList로 요청이 오면 Dao를 호출하여 list를 셋팅하고 cityList.jsp로 포워딩

---
+++ test26/src/main/java/ksmart/project/test26/CityController.java
@@ -0,0 +1,25 @@
+package ksmart.project.test26;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ksmart.project.test26.service.City;
+import ksmart.project.test26.service.CityDao;
+
+@Controller
+public class CityController {
+	@Autowired
+	CityDao cityDao;
+	
+	@RequestMapping(value = "/city/cityList", method = RequestMethod.GET)
+	public String cityList(Model model) {
+		List<City> list = cityDao.selectCityList();
+		model.addAttribute("list", list);
+		return "cityList";
+	}
+}
