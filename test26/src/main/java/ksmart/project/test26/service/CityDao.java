2bf8a0ad47a0c1dd2f67816f4587bfabefdf0205
Administrator <Administrator@smart20105>
2018-02-05 10:46:00 +0900

    (임승권) Dao(Model) INSERT, UPDATE, DELETE 메서드 추가

--- test26/src/main/java/ksmart/project/test26/service/CityDao.java
+++ test26/src/main/java/ksmart/project/test26/service/CityDao.java
@@ -11,7 +11,31 @@ public class CityDao {
 	@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
 	
+	// namespace (중복 처리)
+	private final String NAMESPACE = "ksmart.project.test26.service.CityMapper.";
+	
+	// 전체 도시 목록
 	public List<City> selectCityList() {
-		return sqlSessionTemplate.selectList("ksmart.project.test26.service.CityMapper.selectCityList");
+		return sqlSessionTemplate.selectList(NAMESPACE + "selectCityList");
+	}
+	
+	// 도시 추가
+	public void insertCity(City city) {
+		sqlSessionTemplate.insert(NAMESPACE + "insertCity", city);
+	}
+	
+	// 도시 조회 (수정)
+	public City selectCityName(City city) {
+		return sqlSessionTemplate.selectOne(NAMESPACE + "selectCityName", city);
+	}
+	
+	// 도시 수정
+	public void updateCity(City city) {
+		sqlSessionTemplate.update(NAMESPACE + "updateCity", city);
+	}
+	
+	// 도시 삭제
+	public void deleteCity(City city) {
+		sqlSessionTemplate.delete(NAMESPACE + "deleteCity", city);
 	}
 }
