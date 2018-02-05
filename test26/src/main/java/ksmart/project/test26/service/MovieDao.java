1c3c53b0ccfb082774080d13f5570a2fdee7628c
Administrator <Administrator@smart20105>
2018-02-05 16:09:07 +0900

    (임승권) MovieDao.java INSERT, DELETE, UPDATE 추가

--- test26/src/main/java/ksmart/project/test26/service/MovieDao.java
+++ test26/src/main/java/ksmart/project/test26/service/MovieDao.java
@@ -10,10 +10,32 @@ import org.springframework.stereotype.Repository;
 public class MovieDao {
 	
 	@Autowired
-	private SqlSessionTemplate sst;
+	private SqlSessionTemplate sqlSessionTemplate;
+	// 중복을 피하기 위한 namespace
+	private final String NAMESPACE = "ksmart.project.test26.service.MovieMapper."; 
 	
+	// 영화 삭제 처리
+	public void deleteMovie(int movieId) {
+		sqlSessionTemplate.delete(NAMESPACE + "deleteMovie", movieId);
+	}
+	
+	// 영화 수정 처리
+	public void updateMovie(Movie movie) {
+		sqlSessionTemplate.update(NAMESPACE + "updateMovie", movie);
+	}
+	
+	// 영화 수정 화면
+	public Movie updateMovie(int movieId) {
+		return sqlSessionTemplate.selectOne(NAMESPACE + "selectMovieId", movieId);
+	}
+	
+	// 영화 입력 처리
+	public void insertMovie(String movieName) {
+		sqlSessionTemplate.insert(NAMESPACE + "insertMovie", movieName);
+	}
+	
+	// 영화 전체 목록
 	public List<Movie> selectMovieList(){
-		return sst.selectList("ksmart.project.test26.service.MovieMapper.selectMovieList");
-
+		return sqlSessionTemplate.selectList(NAMESPACE + "selectMovieList");
 	}
 }
