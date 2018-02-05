bd332f4b60d8fedf7c8a345eaea16f4ceadf7e5e
Administrator <Administrator@smart20111>
2018-02-05 15:22:52 +0900

    (황신독) CountryDao.java insertCountry,deleteCountry,updateForm(selectName)

--- test26/src/main/java/ksmart/project/test26/service/CountryDao.java
+++ test26/src/main/java/ksmart/project/test26/service/CountryDao.java
@@ -10,10 +10,23 @@ import org.springframework.stereotype.Repository;
 public class CountryDao {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
+	String namespace = "ksmart.project.test26.service.CountryMapper.";
+	
 	public List<Country> selectCountryList(){
-		System.out.println("01-02CountryDao.java selectCountryList 메서드 호출");	
-		
-		return sqlSessionTemplate.selectList("ksmart.project.test26.service.CountryMapper.selectCountryList");
-		
-	}	
+		System.out.println("01-02 CountryDao.java selectCountryList 메서드 호출");			
+		return sqlSessionTemplate.selectList(namespace+"selectCountryList");		
+	}
+	//insertCountry
+	public int insertCountry(Country country){
+		return sqlSessionTemplate.insert(namespace+"insertCountry",country);
+	}
+	//deleteCountry
+    public int deleteCountry(int countryId) {    	
+        return sqlSessionTemplate.delete(namespace+"deleteCountry",countryId);
+    }
+    //updateForm(selectName)
+    public Country selectCountryName(int countryId) {    	    	
+    	System.out.println(countryId+" :CountryDao.java selectCountryName countryId");
+		return sqlSessionTemplate.selectOne(namespace+"selectCountryName", countryId);
+    }
 }
