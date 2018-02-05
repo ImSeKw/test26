31fe4adb9ea058c352bff6fc9f2b4d8604cc9669
Administrator <Administrator@smart20105>
2018-02-05 16:09:33 +0900

    (임승권) MovieController.java INSERT, UPDATE, DELETE 추가

--- test26/src/main/java/ksmart/project/test26/MovieController.java
+++ test26/src/main/java/ksmart/project/test26/MovieController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ksmart.project.test26.service.Movie;
 import ksmart.project.test26.service.MovieDao;
@@ -14,6 +16,43 @@ import ksmart.project.test26.service.MovieDao;
 public class MovieController {
 	@Autowired
 	private MovieDao movieDao;
+	
+	// 영화 삭제 처리
+	@RequestMapping(value="/movie/deleteMovie", method=RequestMethod.GET)
+	public String deleteMovie(@RequestParam(value="movieId") int movieId) {
+		movieDao.deleteMovie(movieId);
+		return "redirect:/movie/movieList";
+	}
+	
+	// 영화 수정 처리
+	@RequestMapping(value="/movie/updateMovie", method=RequestMethod.POST)
+	public String updateMovie(Movie movie) {
+		movieDao.updateMovie(movie);
+		return "redirect:/movie/movieList";
+	}
+	
+	// 영화 수정 화면
+	@RequestMapping(value="/movie/updateMovie", method=RequestMethod.GET)
+	public String updateMovie(Model model, @RequestParam(value="movieId") int movieId) {
+		Movie movie = movieDao.updateMovie(movieId);
+		model.addAttribute("movie", movie);
+		return "movie/updateMovie";
+	}
+	
+	// 영화 입력 처리
+	@RequestMapping(value="/movie/insertMovie", method=RequestMethod.POST)
+	public String insertMovie(@RequestParam(value="movieName") String movieName) {
+		movieDao.insertMovie(movieName);
+		return "redirect:/movie/movieList";
+	}
+	
+	// 영화 입력 화면
+	@RequestMapping(value="/movie/insertMovie", method=RequestMethod.GET)
+	public String insertMovie() {
+		return "movie/insertMovie";
+	}
+	
+	// 영화 전체 목록
 	@RequestMapping(value="/movie/movieList")
 	public String movieList(Model model) {
 		List<Movie> list = movieDao.selectMovieList();
