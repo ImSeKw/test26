e7291352d85c77575fa079ced68252ef45efc6d9
안영신 <@DESKTOP-UQUOB9R>
2018-02-05 11:11:14 +0900

    (안영신)IdolDao.java insert,update,deltet DAO추가

--- test26/src/main/java/ksmart/project/test26/service/IdolDao.java
+++ test26/src/main/java/ksmart/project/test26/service/IdolDao.java
@@ -10,9 +10,28 @@ import org.springframework.stereotype.Repository;
 public class IdolDao {
 	
 	@Autowired
-	private SqlSessionTemplate sst;
-
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	// 경로설정 중복처리
+	private final String Route = "ksmart.project.test26.service.IdolMapper.";
+	
+	// idolList 조회
 	public List<Idol> selectIdolList() {
-		return sst.selectList("ksmart.project.test26.service.IdolMapper.selectIdolList"); 
+		return sqlSessionTemplate.selectList(Route + "selectIdolListDAO"); 
+	}
+	
+	// idol추가입력
+	public void insertIdol(Idol idol) {
+		sqlSessionTemplate.insert(Route + "insertIdolDAO");
+	}
+	
+	// idol수정처리
+	public void updateIdol(Idol idol) {
+		sqlSessionTemplate.update(Route + "updateIdolDAO");
+	}
+	
+	// idol삭제처리
+	public void deleteIdol(Idol idol) {
+		sqlSessionTemplate.delete(Route + "deleteIdolDAO");
 	}
 }
