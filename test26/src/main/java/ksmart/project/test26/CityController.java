612fc7802575af101274cd8d42a6196504cb416b
Administrator <Administrator@smart20105>
2018-02-05 11:28:09 +0900

    (임승권) Controller INSERT, UPDATE, DELETE 추가

--- test26/src/main/java/ksmart/project/test26/CityController.java
+++ test26/src/main/java/ksmart/project/test26/CityController.java
@@ -16,6 +16,36 @@ public class CityController {
 	@Autowired
 	CityDao cityDao;
 	
+	// 도시 삭제
+	@RequestMapping(value = "/city/deleteCity", method = RequestMethod.POST)
+	public String deleteCity(City city) {
+		cityDao.deleteCity(city);
+		return "redirect:/city/cityList";
+	}
+	
+	// 도시 수정
+	@RequestMapping(value = "/city/updateCity", method = RequestMethod.POST)
+	public String updateCity(City city) {
+		cityDao.updateCity(city);
+		return "redirect:/city/cityList";
+	}
+	
+	// 도시 조회 (수정)
+	@RequestMapping(value = "/city/selectCity", method = RequestMethod.GET)
+	public String selectCityId(City city, Model model) {
+		City reCity = cityDao.selectCityId(city);
+		model.addAttribute("city", reCity);
+		return "city/selectCity";
+	}
+	
+	// 도시 입력
+	@RequestMapping(value = "/city/insertCity", method = RequestMethod.POST)
+	public String insertCity(City city) {
+		cityDao.insertCity(city);
+		return "redirect:/city/cityList";
+	}
+	
+	// 도시 전체 조회
 	@RequestMapping(value = "/city/cityList", method = RequestMethod.GET)
 	public String cityList(Model model) {
 		List<City> list = cityDao.selectCityList();
--- test26/src/main/java/ksmart/project/test26/service/CityDao.java
+++ test26/src/main/java/ksmart/project/test26/service/CityDao.java
@@ -10,32 +10,31 @@ import org.springframework.stereotype.Repository;
 public class CityDao {
 	@Autowired
 	SqlSessionTemplate sqlSessionTemplate;
-	
 	// namespace (중복 처리)
 	private final String NAMESPACE = "ksmart.project.test26.service.CityMapper.";
-	
-	// 전체 도시 목록
-	public List<City> selectCityList() {
-		return sqlSessionTemplate.selectList(NAMESPACE + "selectCityList");
-	}
-	
-	// 도시 추가
-	public void insertCity(City city) {
-		sqlSessionTemplate.insert(NAMESPACE + "insertCity", city);
-	}
-	
-	// 도시 조회 (수정)
-	public City selectCityName(City city) {
-		return sqlSessionTemplate.selectOne(NAMESPACE + "selectCityName", city);
-	}
-	
-	// 도시 수정
-	public void updateCity(City city) {
-		sqlSessionTemplate.update(NAMESPACE + "updateCity", city);
-	}
 	
 	// 도시 삭제
 	public void deleteCity(City city) {
 		sqlSessionTemplate.delete(NAMESPACE + "deleteCity", city);
+	}
+		
+	// 도시 수정
+	public void updateCity(City city) {
+		sqlSessionTemplate.update(NAMESPACE + "updateCity", city);
+	}
+		
+	// 도시 조회 (수정)
+	public City selectCityId(City city) {
+		return sqlSessionTemplate.selectOne(NAMESPACE + "selectCityId", city);
+	}
+		
+	// 도시 추가
+	public void insertCity(City city) {
+		sqlSessionTemplate.insert(NAMESPACE + "insertCity", city);
+	}
+		
+	// 전체 도시 목록
+	public List<City> selectCityList() {
+		return sqlSessionTemplate.selectList(NAMESPACE + "selectCityList");
 	}
 }
