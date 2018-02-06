fefc71fea769c092324e1c0af36999a9c366aaa3
Administrator <Administrator@smart20105>
2018-02-06 11:44:44 +0900

    (임승권) 디버그 및 로그인 순서

--- test26/src/main/java/ksmart/project/test26/HomeController.java
+++ test26/src/main/java/ksmart/project/test26/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+	// System.out.println 대체
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -24,7 +24,20 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
+		String s = "test";
+		logger.debug("{}", s);
+		/*
+		 * 1. logger.debug(member)
+		 * 2. dao(member)
+		 * 3. mapper
+		 * 4. null -> redirect /login
+		 * 5. member -> session.setAttribute("loginMember", member)
+		 * 6. redirect
+		 * 
+		 * login / logout
+		 * 1명 : 회원가입...
+		 * 나머지 : 1/n 인증단계를 적용해야 될 Controller
+		 */
 		return "home";
 	}
-	
 }
