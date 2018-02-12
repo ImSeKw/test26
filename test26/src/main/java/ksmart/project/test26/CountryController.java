dbce2ce586b0e6db5100b92e1d4b0b440f7c8b08
황신독 <@sindock>
2018-02-12 10:40:09 +0900

    (황신독) CountryController.java 로그인처리와 service호출 debug처리

--- test26/src/main/java/ksmart/project/test26/CountryController.java
+++ test26/src/main/java/ksmart/project/test26/CountryController.java
@@ -3,8 +3,11 @@ package ksmart.project.test26;
 
 import ksmart.project.test26.service.Country;
 import ksmart.project.test26.service.CountryDao;
+import ksmart.project.test26.service.CountryService;
 
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,40 +21,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class CountryController {
 	@Autowired
-	private CountryDao countryDao;
+	private CountryService countryService;
 	private static final Logger logger = LoggerFactory.getLogger(CountryController.class);
 
-	//나라리스트
+	//나라 전체 조회
 	@RequestMapping(value="/country/countryList")		
-	public String CountryList(Model model) {		
-		System.out.println("01-01 CountryController.java CountryList 硫붿꽌�뱶 �샇異�");			
-		List<Country> list = countryDao.selectCountryList(); 		
-		model.addAttribute("list", list);		
-		return "country/countryList";		
+	public String CountryList(HttpSession httpSession, Model model) {	
+		String view = null;
+		// 로그인 접근 처리
+		if(httpSession.getAttribute("loginMember")==null) { //세션에 로그인정보가 없다면
+			view = "redirect:/";
+		}else if(httpSession.getAttribute("loginMember")!=null) { //세션에 로그인정보가 있다면
+			List<Country> list = countryService.selectCountryList(); 	
+			model.addAttribute("list", list);		
+			logger.debug("{} : CountryController.java CountryList model", model);
+			view = "country/countryList";
+		}
+		return view;	
 	}
-	//나라추가화면 불러오기
+	//나라입력화면 불러오기
 	@RequestMapping(value="/country/insertCountry",method = RequestMethod.GET)
 	public String CountryInsert() {
-		//System.out.println("01_01 CountryController.java CountryInsert硫붿꽌�뱶 �떎�뻾 �엯�젰 �뤌");
+		//System.out.println("01_01 CountryController.java CountryInsert");
 		return "country/insertCountry";	
 	}
 	//나라추가
 	@RequestMapping(value="/country/insertCountry",method = RequestMethod.POST)
 	public String CountryInsert(Country country) {
-		//System.out.println(country+" :CountryController.java CountryInsertAction�쓽 country媛�");		
-		countryDao.insertCountry(country);		
+		//System.out.println(country+" :CountryController.java CountryInsertAction country");		
+		countryService.insertCountry(country);		
 		return "redirect:/country/countryList";		
 	}
 	//나라삭제
 	@RequestMapping(value="/country/deleteCountry",method=RequestMethod.GET)
 	public String CountryDelete(@RequestParam(value="countryId",required=true)int countryId){
-		countryDao.deleteCountry(countryId);
+		countryService.deleteCountry(countryId);
 		return "redirect:/country/countryList";		
 	}
 	//나라수정을위한 아이디 받기
 	@RequestMapping(value="/country/updateCountry",method=RequestMethod.GET)
 	public String CountryUpdate(Model model,@RequestParam(value="countryId",required=true)int countryId) {
-		Country country = countryDao.selectCountryName(countryId);
+		Country country = countryService.selectCountryName(countryId);
 		model.addAttribute("country", country);	
 		logger.debug("{} : country.getMemberId() CountryUpdate CountryController.java GET", country.getCountryId());
 		logger.debug("{} : country.getCountryName() CountryUpdate CountryController.java GET", country.getCountryName());
@@ -63,7 +73,7 @@ public class CountryController {
 	public String CountryUpdate(Country country) {
 		logger.debug("{} : country.getMemberId() CountryUpdate CountryController.java POST", country.getCountryId());
 		logger.debug("{} : country.getCountryName() CountryUpdate CountryController.java POST", country.getCountryName());
-		countryDao.updateCountry(country);
+		countryService.updateCountry(country);
 
 		return "redirect:/country/countryList";
 	}
