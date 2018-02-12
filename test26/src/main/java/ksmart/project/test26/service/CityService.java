e58c272ba2552c7eeda76e9893bba397b277dc64
Administrator <Administrator@smart20105>
2018-02-12 12:35:37 +0900

    (임승권) Service 도시 조회와 전체 도시 수를 호출하고 페이징 작업 후 Map을 리턴하는 메서드 생성

--- test26/src/main/java/ksmart/project/test26/service/CityService.java
+++ test26/src/main/java/ksmart/project/test26/service/CityService.java
@@ -1,7 +1,11 @@
 package ksmart.project.test26.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,29 +15,64 @@ import org.springframework.transaction.annotation.Transactional;
 public class CityService {
 	@Autowired
 	CityDao cityDao;
+	private static final Logger logger = LoggerFactory.getLogger(CityService.class);
+	
+	// 도시 조회(페이징)
+	public Map<String, Object> selectCityListAndCountByPage(int currentPage, int pagePerRow) {
+		logger.debug("{} : <currentPage selectCityListAndCountByPage CityService", currentPage);
+		logger.debug("{} : <pagePerRow selectCityListAndCountByPage CityService", pagePerRow);
+		// 쿼리문의 시작 페이지
+		int startPage = 0;
+		if(currentPage > 1) {
+			startPage = (currentPage-1)*pagePerRow;
+		}
+		// DAO에 시작 페이지와 행의 수 보내기
+		Map<String, Integer> map = new HashMap();
+		map.put("startPage", startPage);
+		map.put("pagePerRow", pagePerRow);
+		List<City> list = cityDao.selectCityListByPage(map);
+		logger.debug("{} : >list selectCityListAndCountByPage CityService", list);
+		// 총 행의 수를 보여줄 행의 수로 나눈 뒤 나머지가 0일 경우는 넘어가고 아닐 경우 +1 한다.
+		int count = cityDao.selectCityCountByPage();
+		int countPage = count/pagePerRow;
+		if(count%pagePerRow != 0) {
+			countPage++;
+		}
+		logger.debug("{} : >countPage selectCityListAndCountByPage CityService", countPage);
+		// list와 페이지 수를 리턴하기
+		Map<String, Object> returnMap = new HashMap();
+		returnMap.put("list", list);
+		returnMap.put("countPage", countPage);
+		return returnMap;
+	}
 	
 	// 도시 삭제 Action
 	public void deleteCity(City city) {
+		logger.debug("{} : <city deleteCity CityService", city);
 		cityDao.deleteCity(city);
 	}
 	
 	// 도시 수정 Action
 	public void updateCity(City city) {
+		logger.debug("{} : <city updateCity CityService", city);
 		cityDao.updateCity(city);
 	}
 	
 	// 도시 수정 Form
 	public City updateCity(int cityId) {
+		logger.debug("{} : <cityId updateCity CityService", cityId);
 		return cityDao.updateCity(cityId);
 	}
 	
 	// 도시 추가 Action
 	public void insertCity(City city) {
+		logger.debug("{} : <city insertCity CityService", city);
 		cityDao.insertCity(city);
 	}
 	
 	// 도시 전체 조회
 	public List<City> selectCityList() {
+		logger.debug(">selectCityList CityService");
 		return cityDao.selectCityList();
 	}
 }
