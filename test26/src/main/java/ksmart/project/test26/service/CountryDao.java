dcdc4e54af63585decf3febf7f3fd2196e632be4
황신독 <@sindock>
2018-02-12 17:23:37 +0900

    (황신독) CountryDao.java 전체 개수와 페이징작업을 위한 Mapper 호출

--- test26/src/main/java/ksmart/project/test26/service/CountryDao.java
+++ test26/src/main/java/ksmart/project/test26/service/CountryDao.java
@@ -1,6 +1,7 @@
 package ksmart.project.test26.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
@@ -15,29 +16,35 @@ public class CountryDao {
 	private static final Logger logger = LoggerFactory.getLogger(CountryDao.class);
 	// 경로설정 중복처리
 	private final String NAMESPACE = "ksmart.project.test26.mapper.CountryMapper.";
-	
+	// country 전체  개수
+	public int selectCountryCountByPage() {
+		logger.debug("CountryDao.java selectCityListByPage");
+		return sqlSessionTemplate.selectOne(NAMESPACE + "selectCityCountByPage");
+	}
+	// country 페이징 작업 조회
+	public List<Country> selectCountryListByPage(Map<String,Object> map){
+		logger.debug("{} : CountryDao.java selectCityListByPage startPage", map.get("startPage"));
+		logger.debug("{} : CountryDao.java selectCityListByPage pagePerRow", map.get("pagePerRow"));
+		return sqlSessionTemplate.selectList(NAMESPACE + "selectCityListByPage", map);
+	}
 	// 나라 전체 조회
-	public List<Country> selectCountryList(){
+	public List<Country> selectCountryList(){ 
 		return sqlSessionTemplate.selectList(NAMESPACE + "selectCountryList");		
 	}
-	
 	// 나라 입력
 	public void insertCountry(Country country){
 		logger.debug("{} : CountryDao.java insertCountry country", country);
 		sqlSessionTemplate.insert(NAMESPACE + "insertCountry",country);
 	}
-	
 	// 나라 삭제
     public int deleteCountry(int countryId) {    	
         return sqlSessionTemplate.delete(NAMESPACE + "deleteCountry",countryId);
     }
-    
     // 나라 이름 조회
     public Country selectCountryName(int countryId) {    	
 		logger.debug("{} : CountryDao.java selectCountryName countryId", countryId);
 		return sqlSessionTemplate.selectOne(NAMESPACE + "selectCountryName", countryId);
     }
-    
     // 나라 수정
     public void updateCountry(Country country) {
 		sqlSessionTemplate.update(NAMESPACE + "updateCountry",country);
