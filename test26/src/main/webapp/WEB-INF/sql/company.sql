73e55c525c422cb9605c056f9ddb607fd12495fa
Administrator <Administrator@smart20106>
2018-02-12 16:13:06 +0900

    (이원진)company.sql movie.sql 수정

--- test26/src/main/webapp/WEB-INF/sql/company.sql
+++ test26/src/main/webapp/WEB-INF/sql/company.sql
@@ -1,70 +1,42 @@
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        5.5.32 - MySQL Community Server (GPL)
--- 서버 OS:                        Win32
--- HeidiSQL 버전:                  8.0.0.4396
--- --------------------------------------------------------
+DELETE FROM `company`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- test 의 데이터베이스 구조 덤핑
-CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-USE `test`;
-
-
--- 테이블 test의 구조를 덤프합니다. company
-CREATE TABLE IF NOT EXISTS `company` (
-  `company_id` int(10) NOT NULL AUTO_INCREMENT,
-  `company_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '0',
-  PRIMARY KEY (`company_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
-
--- Dumping data for table test.company: ~39 rows (대략적)
-/*!40000 ALTER TABLE `company` DISABLE KEYS */;
-INSERT INTO `company` (`company_id`, `company_name`) VALUES
-	(1, '하이텍크'),
-	(2, 'LG'),
-	(3, '삼성'),
-	(4, 'SK'),
-	(5, '현대'),
-	(6, '한화'),
-	(7, 'sktelecom'),
-	(8, '유한제지'),
-	(9, '롯데'),
-	(10, '엘지전자'),
-	(12, '유한킴벌리'),
-	(13, 'CJ'),
-	(14, '명성식품'),
-	(15, '명성기업'),
-	(16, 'KT'),
-	(17, '명인 철광'),
-	(18, '포스코'),
-	(19, '아마존'),
-	(20, '애플'),
-	(21, '구글'),
-	(22, '페이스북'),
-	(23, '마이크로소프트'),
-	(24, '월마트'),
-	(25, '버라이즌'),
-	(26, '토요타'),
-	(27, '케논'),
-	(28, '소니'),
-	(29, 'BP'),
-	(30, 'AT&T'),
-	(31, 'TOTAL'),
-	(32, 'COASTCO'),
-	(33, '중국은행'),
-	(34, 'BMW GROUP'),
-	(35, 'IBM'),
-	(36, 'TESCO'),
-	(37, 'SK HOLDDINGS'),
-	(38, 'PHILIPS'),
-	(39, '금호 타이어'),
-	(40, 'AIG');
-/*!40000 ALTER TABLE `company` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+INSERT INTO `company` (`company_name`) VALUES
+	('하이텍크'),
+	('LG'),
+	('삼성'),
+	('SK'),
+	('현대'),
+	('한화'),
+	('sktelecom'),
+	('유한제지'),
+	('롯데'),
+	('엘지전자'),
+	('유한킴벌리'),
+	('CJ'),
+	('명성식품'),
+	('명성기업'),
+	('KT'),
+	('명인 철광'),
+	('포스코'),
+	('아마존'),
+	('애플'),
+	('구글'),
+	('페이스북'),
+	('마이크로소프트'),
+	('월마트'),
+	('버라이즌'),
+	('토요타'),
+	('케논'),
+	('소니'),
+	('BP'),
+	('AT&T'),
+	('TOTAL'),
+	('COASTCO'),
+	('중국은행'),
+	('BMW GROUP'),
+	('IBM'),
+	('TESCO'),
+	('SK HOLDDINGS'),
+	('PHILIPS'),
+	('금호 타이어'),
+	('AIG');
--- test26/src/main/webapp/WEB-INF/sql/movie.sql
+++ test26/src/main/webapp/WEB-INF/sql/movie.sql
@@ -1,70 +1,42 @@
--- --------------------------------------------------------
--- 호스트:                          127.0.0.1
--- 서버 버전:                        5.5.32 - MySQL Community Server (GPL)
--- 서버 OS:                        Win32
--- HeidiSQL 버전:                  8.0.0.4396
--- --------------------------------------------------------
+DELETE FROM `movie`;
+INSERT INTO `movie` (`movie_name`) VALUES
+	('코코코'),
+	('1987'),
+	('신과함께'),
+	('라라랜드'),
+	('세 얼간이'),
+	('드래고 길들이기'),
+	('클래식'),
+	('집으로 가는길'),
+	('여인의 향기'),
+	('소원'),
+	('에이리언 2'),
+	('헬프'),
+	('모노노케 히메'),
+	('글래디에이터'),
+	('라푼젤'),
+	('집으로'),
+	('아이언 자이언트'),
+	('캐스트 어웨이'),
+	('프리쿼시'),
+	('해리포터아 죽음의 성물-2부'),
+	('모던 타임즈'),
+	('바이센테니얼 맨'),
+	('코러스'),
+	('내 이름은 칸'),
+	('스피릿'),
+	('오세암'),
+	('다크 나이트'),
+	('시네마 천국'),
+	('굿 월 헌팅'),
+	('영웅'),
+	('블라인드 사이드'),
+	('아마데우스'),
+	('서유기'),
+	('트루먼쇼'),
+	('타잔'),
+	('사라은 비를 타고'),
+	('더록'),
+	('아이엠 샘'),
+	('록키');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- test 의 데이터베이스 구조 덤핑
-CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-USE `test`;
-
-
--- 테이블 test의 구조를 덤프합니다. movie
-CREATE TABLE IF NOT EXISTS `movie` (
-  `movie_id` int(10) NOT NULL AUTO_INCREMENT,
-  `movie_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '0',
-  PRIMARY KEY (`movie_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
-
--- Dumping data for table test.movie: ~39 rows (대략적)
-/*!40000 ALTER TABLE `movie` DISABLE KEYS */;
-INSERT INTO `movie` (`movie_id`, `movie_name`) VALUES
-	(3, '코코코'),
-	(4, '1987'),
-	(5, '신과함께'),
-	(7, '라라랜드'),
-	(8, '세 얼간이'),
-	(9, '드래고 길들이기'),
-	(10, '클래식'),
-	(11, '집으로 가는길'),
-	(12, '여인의 향기'),
-	(13, '소원'),
-	(14, '에이리언 2'),
-	(15, '헬프'),
-	(16, '모노노케 히메'),
-	(17, '글래디에이터'),
-	(18, '라푼젤'),
-	(19, '집으로'),
-	(20, '아이언 자이언트'),
-	(21, '캐스트 어웨이'),
-	(22, '프리쿼시'),
-	(23, '해리포터아 죽음의 성물-2부'),
-	(24, '모던 타임즈'),
-	(25, '바이센테니얼 맨'),
-	(26, '코러스'),
-	(27, '내 이름은 칸'),
-	(28, '스피릿'),
-	(29, '오세암'),
-	(30, '다크 나이트'),
-	(31, '시네마 천국'),
-	(32, '굿 월 헌팅'),
-	(33, '영웅'),
-	(34, '블라인드 사이드'),
-	(35, '아마데우스'),
-	(36, '서유기'),
-	(37, '트루먼쇼'),
-	(38, '타잔'),
-	(39, '사라은 비를 타고'),
-	(40, '더록'),
-	(41, '아이엠 샘'),
-	(42, '록키');
-/*!40000 ALTER TABLE `movie` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
