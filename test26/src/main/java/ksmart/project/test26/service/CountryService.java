1887bc44e226bbdb4a545c618f3984119b0f6350
황신독 <@sindock>
2018-02-14 16:50:29 +0900

    (황신독) CountryService.java 페이징에 필요한 변수 정의와 변수명 통일

--- test26/src/main/java/ksmart/project/test26/service/CountryService.java
+++ test26/src/main/java/ksmart/project/test26/service/CountryService.java
@@ -15,73 +15,71 @@ import org.springframework.transaction.annotation.Transactional;
 public class CountryService {
 	@Autowired
 	CountryDao countryDao;
-	private int count;
-	private int startPage;
-	private int endPage;
-	private boolean prev;
-	private boolean next;
 	private static final Logger logger = LoggerFactory.getLogger(CountryService.class);
-	//나라 전체 조회
-	//public List<Country>selectCountryList(int page, String word){	
-	public Map<String, Object>selectCountryListAndCountByPage(int currentPage,int pagePerRow,String word ){	
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage currentPage", currentPage);
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage pagePerRow", pagePerRow);
+	
+	// 도시 조회(페이징)
+	public Map<String, Object> selectCountryListAndCountByPage(int currentPage, int pagePerRow, String word) {
+		logger.debug("{} : CountryService selectCountryListAndCountByPage currentPage", currentPage);
+		logger.debug("{} : CountryService selectCountryListAndCountByPage pagePerRow", pagePerRow);
+		logger.debug("{} : CountryService selectCountryListAndCountByPage word", word);
 		// 쿼리문의 시작 페이지
-
-		endPage = (int)(Math.ceil(currentPage/(double)pagePerRow)*pagePerRow);//endPage 를 구한다.
-		startPage = (endPage - pagePerRow)+1; // 5의 배수로 startPage가 나뉠 때를 구한 startPage
-		count = countryDao.selectCountryCountByPage(); //전체 개수
-		int tempEndPage = (int)(Math.ceil(count/(double)pagePerRow)); //마지막 페이지
-		if(endPage > tempEndPage) { //마지막 페이지를 endPage로 할당한다
-			endPage = tempEndPage;
+		int startPage = 0;
+		if(currentPage > 1) {
+			startPage = (currentPage-1)*pagePerRow;
 		}
-		prev = startPage == 1?false : true;
-		next = endPage*pagePerRow>count?false : true;
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage endPage", endPage);
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage startPage", startPage);
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage tempEndPage", tempEndPage);
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage count", count);
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage prev", prev);
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage next", next);
-		Map<String,Object> map = new HashMap<String,Object>();
+		// DAO에 시작 페이지와 행의 수 보내기
+		Map<String, Object> map = new HashMap();
 		map.put("startPage", startPage);
-		map.put("pagePerRow",pagePerRow);
+		map.put("pagePerRow", pagePerRow);
 		map.put("word", word);
-		// CountryDao에 startPage,pagePerRow보내기
 		List<Country> list = countryDao.selectCountryListByPage(map);
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage list", list);
+		logger.debug("{} : CountryService selectCountryListAndCountByPage list", list);
 		// 총 행의 수를 보여줄 행의 수로 나눈 뒤 나머지가 0일 경우는 넘어가고 아닐 경우 +1 한다.
+		int count = countryDao.selectCountryCountByPage(map);
+		logger.debug("{} : CountryService selectCountryCountByPage count", count);
 		int countPage = count/pagePerRow;
 		if(count%pagePerRow != 0) {
 			countPage++;
 		}
-		logger.debug("{} : CountryService.java selectCountryListAndCountByPage countPage", countPage);
-		//list와 page 수를 리턴
-		Map<String,Object> reMap = new HashMap<String, Object>();
-		reMap.put("list", list);
-		reMap.put("countPage", countPage);
-		reMap.put("endPage", endPage);
-		reMap.put("startPage", startPage);
-		reMap.put("count", count);
-		reMap.put("prev", prev);
-		reMap.put("next", next);
-		return reMap;		
+		logger.debug("{} : CountryService selectCountryCountByPage countPage", countPage);
+		// 자신의 카테고리 셋팅
+		String category = "country";			
+		// list와 페이지 수를 리턴하기
+		Map<String, Object> returnMap = new HashMap();
+		returnMap.put("list", list);
+		returnMap.put("countPage", countPage);
+		returnMap.put("word", word);
+		returnMap.put("category", category);
+		return returnMap;
 	}
-	//나라 입력 Action
-	public void insertCountry(Country country) {
-		countryDao.insertCountry(country);
+	
+	// 도시 삭제 Action
+	public void deleteCountry(Country country) {
+		logger.debug("{} : CountryService deleteCountry country", country);
+		countryDao.deleteCountry(country);
 	}
-	//나라 수정 Form
-	public Country selectCountryName(int countryId) {
-		return countryDao.selectCountryName(countryId);
-	}
-	//나라 수정 Action
+	
+	// 도시 수정 Action
 	public void updateCountry(Country country) {
+		logger.debug("{} : CountryService updateCountry country", country);
 		countryDao.updateCountry(country);
 	}
-	//나라 삭제 Action
-	public void deleteCountry(int countryId) {
-		countryDao.deleteCountry(countryId);
+	
+	// 도시 수정 Form
+	public Country updateCountry(int countryId) {
+		logger.debug("{} : CountryService updateCountry countryId", countryId);
+		return countryDao.updateCountry(countryId);
 	}
-
+	
+	// 도시 추가 Action
+	public void insertCountry(Country country) {
+		logger.debug("{} : CountryService updateCountry country", country);
+		countryDao.insertCountry(country);
+	}
+	
+	// 도시 전체 조회
+	public List<Country> selectCountryList() {
+		logger.debug("CountryService selectCountryList ");
+		return countryDao.selectCountryList();
+	}
 }
